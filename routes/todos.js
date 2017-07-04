306ec2c6f32d0dddc7d00dbe2582d8eeae33564a
wangruilong <862452159@qq.com>
2017-07-04 15:25:44 +0800

    测试每个方法

--- routes/todos.js
+++ routes/todos.js
@@ -7,11 +7,43 @@ let Todo = require('../models/Todo.js');
 
 // GET
 router.get('/', function(req, res, next) {
-  console.log("1111111")
   Todo.find(function (err, todos) {
     if (err) return next(err);
     res.json(todos);
   });
 });
+
+// POST
+router.post('/', function(req, res, next) {
+  Todo.create(req.body,function (err, todos) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// GET 传id
+router.get('/:id',function(req,res,next){
+    Todo.findById(req.params.id,function(err,post){
+        if(err) return next(err);
+        res.json(post);
+    })
+})
+
+// PUT 传id
+router.put('/:id',function(req,res,next){
+    Todo.findByIdAndUpdate(req.params.id,req.body,function(err,post){
+        if(err) return next(err);
+        res.json(post)
+    })
+})
+
+// DELETE 传id
+router.delete('/:id',function(req,res,next){
+    Todo.findByIdAndRemove(req.params.id,req.body,function(err,post){
+        if(err) return next(err);
+        res.json(post)
+    })
+})
+
 
 module.exports = router;
