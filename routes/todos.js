3e3ff6ee9cde807bf514eb6edda4e8e321e5b028
wangruilong <862452159@qq.com>
2017-07-05 17:19:35 +0800

    测试几个接口

--- routes/todos.js
+++ routes/todos.js
@@ -2,14 +2,51 @@ var express = require('express');
 var router = express.Router();
 
 var Todo = require('../models/Todo.js');
+var User = require('../models/User.js');
 
 /* GET /todos listing. */
 router.get('/getList', function(req, res, next){
   Todo.find(function (err, resp) {
     if (err) return next(err);
-    res.json(resp[0]);
+    res.json(resp);
   });
 });
+
+
+
+router.get('/addUser', function(req, res, next){
+  let newUser = new User({
+    name:"王睿龙2号",
+    sex:0,
+    age:24,
+    date:new Date()
+  })
+  console.log(req.body)
+  newUser.save(function (err, resp) {
+    if (err) return next(err);
+    res.json(resp);
+  });
+});
+
+
+  router.get('/getUser', function(req, res, next){
+    User.find(function (err, resp) {
+      if (err) return next(err);
+      res.json(resp);
+    });
+  });
+  
+  router.get('/updateUser', function(req, res, next){
+    let before = {"name":"王睿龙"};
+    let update = {"age":33}
+    User.update(before,update,function (err, resp) {
+      if (err) return next(err);
+      res.json(resp);
+    });
+  });
+
+
+
 
 /* POST /todos */
 router.post('/', function(req, res, next){
