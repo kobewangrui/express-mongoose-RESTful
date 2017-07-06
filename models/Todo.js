f97585fe85896b7bf5ed53f9d17574da32a886db
wangruilong <862452159@qq.com>
2017-07-06 17:28:27 +0800

    备注对model的理解

--- models/Todo.js
+++ models/Todo.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var TodoSchema = new mongoose.Schema({
   name: String,
   completed: Boolean,
-  note: String
+  date: Date
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
