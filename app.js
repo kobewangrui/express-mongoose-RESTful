517d9a3c8d778a34281c85cd6fe3cffad0ab8ef0
wangruilong <862452159@qq.com>
2017-07-25 15:18:31 +0800

    设置允许跨域访问

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-
-
 var app = express();
+
+// 设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 
 // 引入连接mongoDB模块
 require('./connect/')
