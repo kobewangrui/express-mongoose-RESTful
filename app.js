d4131bcca59fdf82ee8c7301577b14c2656ea78a
wangruilong <kobewangrui@163.com>
2017-12-06 14:23:42 +0800

    后端解决跨域，代码存在问题，修改

--- app.js
+++ app.js
@@ -9,14 +9,13 @@ var session = require('express-session');
 var app = express();
 
 // 设置跨域访问
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
+// app.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");//一般不写*，写域名
+//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//     res.header("Access-Control-Request-Headers","X-requested-with");    
+//     res.header("Access-Control-Allow-Headers","Content-Type");
+//     next();
+// });
 
 // 引入连接mongoDB模块
 require('./connect/')
